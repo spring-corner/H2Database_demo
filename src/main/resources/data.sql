8a4bd9b6991877044d98656778cb8a79b1424464
Teo Jia Cheng <jcteo.2018@scis.smu.edu.sg>
2024-12-02 19:13:25 +0800

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into actor_title(id, name) values (1, 'actor1');
+insert into actor_title(id, name) values (2, 'actor2');
+insert into actor_title(id, name) values (3, 'actor3');
+insert into actor_title(id, name) values (4, 'actor4');
+insert into actor_title(id, name) values (5, 'actor5');
